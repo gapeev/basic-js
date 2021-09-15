e204fcc70b141ea32cea786d98aacdd0ab9ddd6a
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-09-15 16:43:32 +0300

    feat: implement chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,36 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.values.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.values = [...(this.values || []), value];
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      !Number.isInteger(position) ||
+      position < 1 ||
+      position > this.values.length
+    ) {
+      this.values = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.values = this.values.filter((_, index) => index !== position - 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.values.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const chain = this.values.map((value) => `( ${value} )`).join("~~");
+    this.values = [];
+    return chain;
+  },
 };
