2d673c6181a0682a5f94d58cf57f62000b87bd20
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-09-16 09:07:38 +0300

    feat: implement getCommonCharacterCount function

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  if (s1.length === 0 || s2.length === 0) {
+    return 0;
+  }
+
+  const index = s2.indexOf(s1[0]);
+  if (index === -1) {
+    return getCommonCharacterCount(s1.substring(1), s2);
+  }
+
+  return (
+    1 +
+    getCommonCharacterCount(
+      s1.substring(1),
+      s2.substring(0, index) + s2.substring(index + 1)
+    )
+  );
 }
