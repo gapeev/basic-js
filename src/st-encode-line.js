934407a0a35dc6a14c03a68d8e4443fd0a1eede6
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-09-16 14:45:24 +0300

    feat: implement encodeLine function

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +10,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  if (str.length === 0) {
+    return str;
+  }
+
+  const iter = (index, count, acc) => {
+    if (index > str.length) {
+      return acc;
+    }
+    return iter(
+      index + 1,
+      str[index] !== str[index - 1] ? 1 : count + 1,
+      str[index] !== str[index - 1]
+        ? `${acc}${count === 1 ? "" : count}${str[index - 1]}`
+        : acc
+    );
+  };
+  return iter(1, 1, "");
 }
